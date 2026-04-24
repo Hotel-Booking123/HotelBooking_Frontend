eb60feeb55f8cedf48ce4aa8bf62f3cb81e76de1
Harshiniii027 <gmdhguruharshini@gmail.com>
2026-04-24 11:43:45 +0530

    first commit

---
+++ src/app/shared/models/model.ts
@@ -0,0 +1,63 @@
+export interface User {
+  id: number;
+  fullName: string;
+  email: string;
+  role: string;
+  createdAt: Date;
+}
+
+export interface Hotel {
+  id: number;
+  name: string;
+  location: string;
+  description: string;
+  amenities: string;
+  averageRating: number;
+}
+
+export interface Room {
+  id: number;
+  roomNumber: string;
+  type: string;
+  pricePerNight: number;
+  maxOccupancy: number;
+  hotelId: number;
+  hotel?: Hotel;
+}
+
+export interface Booking {
+  id: number;
+  bookingReference: string;
+  hotelName: string;
+  roomNumber: string;
+  roomType: string;
+  checkInDate: Date;
+  checkOutDate: Date;
+  totalPrice: number;
+  status: string;
+  bookingDate: Date;
+  discountCode?: string;
+  userName?: string;
+  userEmail?: string;
+  userId?: number;
+  roomId?: number;
+  room?: Room;
+  user?: User;
+}
+
+export interface Promotion {
+  id: number;
+  code: string;
+  discountType: string;
+  discountValue: number;
+  validFrom: Date;
+  validTo: Date;
+  isActive: boolean;
+}
+
+export interface AuthResponse {
+  token: string;
+  email: string;
+  role: string;
+  fullName: string;
+}
